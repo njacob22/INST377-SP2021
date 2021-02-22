363ff56094c4b436fe61d0feb7385b77b4b8cba6
njacob22 <78120172+njacob22@users.noreply.github.com>
2021-02-22 15:56:18 -0500

    lab 3 update
    css complete

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,17 @@
-/* Put your javascript in here */
+// let width = 130
+// let count = 3
+// let position = 0
+
+// let picsList = document.querySelector('ul')
+// let pics = document.querySelectorAll('li')
+
+// carousel.querySelector('.right').onclick() = function() {
+//   position -= width * count
+//   position = Math.max(position, -width * (pics.length - count))
+//   picsList.style.marginLeft = position + 'px';
+// }
+// document.querySelector('.left').onclick() = function() {
+//   position += width * count;
+//   position = Math.min(position,0);
+//   picsList.style.marginLeft = position + 'px';
+// }
