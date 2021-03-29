1ff5edf5247eacf56df3125a5b92b4cf8e645d7a
njacob22 <78120172+njacob22@users.noreply.github.com>
2021-03-29 12:56:36 -0400

    lab 3
    finished

---
+++ public/labs/lab_3/sample.js
@@ -0,0 +1,17 @@
+// let width = 130
+// let count = 3
+// let position = 0
+
+// let picsList = document.querySelector('ul')
+// let pics = document.querySelectorAll('li')
+
+// carousel.querySelector('.right').onclick() = function() {
+//   position -= width * count
+//   position = Math.max(position, -width * (pics.length - count))
+//   picsList.style.marginLeft = position + 'px';
+// }
+// document.querySelector('.left').onclick() = function() {
+//   position += width * count;
+//   position = Math.min(position,0);
+//   picsList.style.marginLeft = position + 'px';
+//
--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,17 +1,22 @@
-// let width = 130
-// let count = 3
-// let position = 0
+let position = 0;
+const width = 130;
+const count = 3;
 
-// let picsList = document.querySelector('ul')
-// let pics = document.querySelectorAll('li')
+const picsList = document.querySelector('ul');
+// let pics = document.querySelectorAll('li');
 
-// carousel.querySelector('.right').onclick() = function() {
-//   position -= width * count
-//   position = Math.max(position, -width * (pics.length - count))
-//   picsList.style.marginLeft = position + 'px';
-// }
-// document.querySelector('.left').onclick() = function() {
-//   position += width * count;
-//   position = Math.min(position,0);
-//   picsList.style.marginLeft = position + 'px';
-// }
+function carousel() {
+  const next = document.querySelector('#right');
+  next.addEventListener('click', (event) => {
+    position -= width * count;
+    picsList.style.marginLeft = `${position}px`;
+  });
+
+  const prev = document.querySelector('#left');
+  prev.addEventListener('click', (event) => {
+    position += width * count;
+    picsList.style.marginLeft = `${position}px`;
+  });
+}
+
+window.onload = carousel;
